ee206c2beb2a458f2200963f0fd9a32fbb6a9949
DzakyFaishalariq <84628553+dzakyfaishalariq@users.noreply.github.com>
2022-02-27 19:23:18 +0700

    tipe data

---
+++ tipe_data.php
@@ -0,0 +1,14 @@
+// tipe date
+<?php
+echo "Desimal : ";
+var_dump(1234);
+echo "octal : ";
+var_dump(0755);
+echo "Biner : ";
+var_dump(0b11111111);
+echo "Hexa : ";
+var_dump(0xFF);
+echo "undescore : ";
+var_dump(2_147_483_647);
+echo "float : ";
+var_dump(1.23);
